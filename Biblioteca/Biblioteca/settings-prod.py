095b7e0f7085da1acd0bcf850021eba4fc72d682
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2019-10-19 12:11:54 -0500

    Agregando configuración para hacer uso de MySQL como base de datos en settings-prod.py

--- Biblioteca/Biblioteca/settings-prod.py
+++ Biblioteca/Biblioteca/settings-prod.py
@@ -79,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'rctorr',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'dalaia12.mysql.pythonanywhere-services.com',
     }
 }
 
